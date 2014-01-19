3531b5e893a3097fc4b3cf7014ba936bab335edb
Artem Dudarev <dudarev@gmail.com>
2014-01-19 18:42:23 +0200

    frontpage app with home page

--- cncdnua/cncdnua/urls.py
+++ cncdnua/cncdnua/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cncdnua.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'', include('frontpage.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
